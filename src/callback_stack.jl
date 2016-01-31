4f84172854d2ea979a1fcdad1ea2bd81563c8f6a
thomlake <thom.l.lake@gmail.com>
2016-01-31 14:49:38 -0600

    trying to get gc to work, no luck

--- src/callback_stack.jl
+++ src/callback_stack.jl
@@ -8,13 +8,6 @@ function backprop!(stack::CallbackStack)
     empty!(stack)
 end
 
-# function gradient!(f::Function, params::Component, args...)
-#     stack = CallbackStack()
-#     y = f(stack, params, args...)
-#     backprop!(stack)
-#     return y
-# end
-
 function gradient!(f::Function, args...)
     stack = CallbackStack()
     y = f(stack, args...)

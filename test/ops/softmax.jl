a90f23dc6c4bdc0c5206b67c6076ee197eaf389e
thomlake <thom.l.lake@gmail.com>
2016-02-02 13:28:42 -0600

    everything broken

--- test/ops/softmax.jl
+++ test/ops/softmax.jl
@@ -1,113 +1,60 @@
 using Flimsy
 
 facts("softmax") do
-    context("Mx1") do
-        m, n = 5, 1
-        context("Array") do
-            x = randn(m)
-            y = softmax(x)
-            @fact isa(y, Vector) --> true
-            @fact size(y)        --> (m,)
-            @fact y              --> roughly(exp(x) / sum(exp(x)))
-            @fact sum(y)         --> roughly(1)
-        end
+    for (m, n) in [(3, 1), (4, 7)]
+        context("$(m)x$(n)") do
+            context("Array") do
+                x = randn(m, n)
+                y = softmax(x)
+                @fact isa(y, Matrix) --> true
+                @fact size(y)        --> (m, n)
+                @fact y              --> roughly(exp(x) ./ sum(exp(x), 1))
+                @fact sum(y, 1)      --> roughly(ones(1, n))
+                @fact sum(y)         --> roughly(n)
+            end
 
-        context("Scope") do
-            scope = DynamicScope()
-            
-            x = DataVariable(randn(m))
-            y = softmax(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data)          --> roughly(1)
-            
-            x = GradVariable(randn(m))
-            y = softmax(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data)          --> roughly(1)
-        end
+            context("Scope") do
+                scope = DynamicScope()
+                x = DataVariable(randn(m, n))
+                y = softmax(scope, x)
+                @fact isa(y, DataVariable) --> true
+                @fact size(y)              --> (m, n)
+                @fact y.data               --> roughly(softmax(x.data))
+                @fact sum(y.data, 1)       --> roughly(ones(1, n))
+                @fact sum(y.data)          --> roughly(n)
 
-        context("GradScope") do
-            scope = GradScope(DynamicScope())
+                x = GradVariable(randn(m, n), zeros(m, n))
+                y = softmax(scope, x)
+                @fact isa(y, DataVariable) --> true
+                @fact size(y)              --> (m, n)
+                @fact y.data               --> roughly(softmax(x.data))
+                @fact sum(y.data, 1)       --> roughly(ones(1, n))
+                @fact sum(y.data)          --> roughly(n)
+            end
 
-            x = DataVariable(randn(m))
-            y = softmax(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data)          --> roughly(1)
+            context("GradScope") do
+                scope = GradScope(DynamicScope())
+                x = DataVariable(randn(m, n))
+                y = softmax(scope, x)
+                @fact isa(y, DataVariable) --> true
+                @fact size(y)              --> (m, n)
+                @fact y.data               --> roughly(softmax(x.data))
+                @fact sum(y.data, 1)       --> roughly(ones(1, n))
+                @fact sum(y.data)          --> roughly(n)
 
-            x = GradVariable(randn(m))
-            y = softmax(scope, x)
-            @fact isa(y, GradVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data)          --> roughly(1)
-        end
+                x = GradVariable(randn(m, n), zeros(m, n))
+                y = softmax(scope, x)
+                @fact isa(y, GradVariable) --> true
+                @fact size(y)              --> (m, n)
+                @fact y.data               --> roughly(softmax(x.data))
+                @fact sum(y.data, 1)       --> roughly(ones(1, n))
+                @fact sum(y.data)          --> roughly(n)
+            end
 
-        context("Gradient") do
-            x = GradVariable(randn(m))
-            test_op_grad_mse(softmax, x, wrt=x)
-        end
-    end
-    
-
-    context("MxN") do
-        m, n = 4, 7
-        context("Array") do
-            x = randn(m, n)
-            y = softmax(x)
-            @fact isa(y, Matrix) --> true
-            @fact size(y)        --> (m, n)
-            @fact y              --> roughly(exp(x) ./ sum(exp(x), 1))
-            @fact sum(y, 1)      --> roughly(ones(1, n))
-            @fact sum(y)         --> roughly(n)
-        end
-
-        context("Scope") do
-            scope = DynamicScope()
-            x = DataVariable(randn(m, n))
-            y = softmax(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data, 1)       --> roughly(ones(1, n))
-            @fact sum(y.data)          --> roughly(n)
-
-            x = GradVariable(randn(m, n))
-            y = softmax(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data, 1)       --> roughly(ones(1, n))
-            @fact sum(y.data)          --> roughly(n)
-        end
-
-        context("GradScope") do
-            scope = GradScope(DynamicScope())
-            x = DataVariable(randn(m, n))
-            y = softmax(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data, 1)       --> roughly(ones(1, n))
-            @fact sum(y.data)          --> roughly(n)
-
-            x = GradVariable(randn(m, n))
-            y = softmax(scope, x)
-            @fact isa(y, GradVariable) --> true
-            @fact size(y)              --> (m, n)
-            @fact y.data               --> roughly(softmax(x.data))
-            @fact sum(y.data, 1)       --> roughly(ones(1, n))
-            @fact sum(y.data)          --> roughly(n)
-        end
-
-        context("Gradient") do
-            x = GradVariable(randn(m, n))
-            test_op_grad_mse(softmax, x, wrt=x)
+            context("Gradient") do
+                x = GradVariable(randn(m, n), zeros(m, n))
+                test_op_grad_mse(softmax, x, wrt=x)
+            end
         end
     end
 end
--- test/ops/tanh.jl
+++ test/ops/tanh.jl
@@ -1,76 +1,41 @@
 
 facts("tanh") do
-    context("Mx1") do
-        m, n = 3, 1
-        context("Scope") do
-            scope = DynamicScope()
-            x = DataVariable(randn(m))
-            y = tanh(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y) --> (m, n) 
-            @fact y.data --> roughly(tanh(x.data))
+    for (m, n) in [(3, 1), (5, 8)]
+        context("$(m)x$(n)") do
+            context("Scope") do
+                scope = DynamicScope()
+                x = DataVariable(randn(m, n))
+                y = tanh(scope, x)
+                @fact isa(y, DataVariable) --> true
+                @fact size(y) --> (m, n) 
+                @fact y.data --> roughly(tanh(x.data))
 
-            x = GradVariable(randn(m))
-            y = tanh(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y) --> (m, n)
-            @fact y.data --> roughly(tanh(x.data))
+                x = GradVariable(randn(m, n), zeros(m, n))
+                y = tanh(scope, x)
+                @fact isa(y, DataVariable) --> true
+                @fact size(y) --> (m, n)
+                @fact y.data --> roughly(tanh(x.data))
+            end
+
+            context("GradScope") do
+                scope = GradScope(DynamicScope())
+                x = DataVariable(randn(m, n))
+                y = tanh(scope, x)
+                @fact isa(y, DataVariable) --> true
+                @fact size(y) --> (m, n)
+                @fact y.data --> roughly(tanh(x.data))
+
+                x = GradVariable(randn(m, n), zeros(m, n))
+                y = tanh(scope, x)
+                @fact isa(y, GradVariable) --> true
+                @fact size(y) --> (m, n)
+                @fact y.data --> roughly(tanh(x.data))
+            end
+
+            context("Gradient") do
+                x = GradVariable(randn(m, n), zeros(m, n))
+                test_op_grad_mse(tanh, x, wrt=x)
+            end
         end
-
-        context("GradScope") do
-            scope = GradScope(DynamicScope())
-            x = DataVariable(randn(m))
-            y = tanh(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y) --> (m, n)
-            @fact y.data --> roughly(tanh(x.data))
-
-            x = GradVariable(randn(m))
-            y = tanh(scope, x)
-            @fact isa(y, GradVariable) --> true
-            @fact size(y) --> (m, n)
-            @fact y.data --> roughly(tanh(x.data))
-        end
-
-        context("Gradient") do
-            x = GradVariable(randn(m))
-            test_op_grad_mse(tanh, x, wrt=x)
-        end
-    end
-
-    context("MxN") do
-        m, n = 5, 8
-        context("Scope") do
-            scope = DynamicScope()
-            x = DataVariable(randn(m, n))
-            y = tanh(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y) --> (m, n)
-            @fact y.data --> roughly(tanh(x.data))
-
-            x = GradVariable(randn(m, n))
-            y = tanh(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y) --> (m, n)
-            @fact y.data --> roughly(tanh(x.data))
-        end
-
-        context("GradScope") do
-            scope = GradScope(DynamicScope())
-            x = DataVariable(randn(m, n))
-            y = tanh(scope, x)
-            @fact isa(y, DataVariable) --> true
-            @fact size(y) --> (m, n)
-            @fact y.data --> roughly(tanh(x.data))
-
-            x = GradVariable(randn(m, n))
-            y = tanh(scope, x)
-            @fact isa(y, GradVariable) --> true
-            @fact size(y) --> (m, n)
-            @fact y.data --> roughly(tanh(x.data))
-        end
-
-        x = GradVariable(randn(m, n))
-        test_op_grad_mse(tanh, x, wrt=x)
     end
 end

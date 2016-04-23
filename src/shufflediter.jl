ba6c6d72510ad857419107b9bb37ef4162846dfb
thomlake <thom.l.lake@gmail.com>
2016-04-23 08:55:54 -0500

    add methods to make type inference work for ShuffledIter

--- src/shufflediter.jl
+++ src/shufflediter.jl
@@ -4,7 +4,8 @@ type ShuffledIter{T}
     indices::Vector{Int}
 end
 ShuffledIter(data::Vector) = ShuffledIter(data, collect(1:length(data)))
-
 Base.start(iter::ShuffledIter) = (shuffle!(iter.indices); 1)
-Base.next{T}(iter::ShuffledIter{T}, i) = (iter.data[iter.indices[i]], i + 1)
-Base.done(iter::ShuffledIter, i) = i > length(iter.indices)
+Base.next{T}(iter::ShuffledIter{T}, i::Int) = (convert(T, iter.data[iter.indices[i]]), i + 1)
+Base.done(iter::ShuffledIter, i::Int) = i > length(iter.data)
+Base.length(iter::ShuffledIter) = length(iter.data)
+Base.eltype{T}(::Type{ShuffledIter{T}}) = T
